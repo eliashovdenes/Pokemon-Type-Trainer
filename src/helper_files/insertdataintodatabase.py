67a39ac3a03cc455b8792e8c83023f3a63a21f95
Elias.Hovdenes <elias.hovdenes@student.uib.no>
2024-07-28 13:19:38 +0200

    fixed database

--- src/helper_files/insertdataintodatabase.py
+++ src/helper_files/insertdataintodatabase.py
@@ -8,6 +8,17 @@ load_dotenv()
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 
+import sqlite3
+
+conn = sqlite3.connect('./backup.db')
+cursor = conn.cursor()
+
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+tables = cursor.fetchall()
+print(tables)
+
+conn.close()
+
 def copy_table_data(sqlite_cursor, pg_conn, table_name):
     sqlite_cursor.execute(f"SELECT * FROM {table_name}")
     rows = sqlite_cursor.fetchall()
